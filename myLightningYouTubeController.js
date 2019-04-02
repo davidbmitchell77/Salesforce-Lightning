fcfed7be9e63a8317c23e5ea04ab5ae183a33670
David Mitchell <davidbmitchell77@outlook.com>
2019-04-02 08:46:37 -0700

    Update myLightningYouTubeController.js

--- myLightningYouTubeController.js
+++ myLightningYouTubeController.js
@@ -3,5 +3,6 @@
     {
         component.set('v.frameWidth', (window.innerWidth - 10) + 'px');
         component.set('v.frameHeight', (window.innerHeight - 10) + 'px');
+        component.set('v.url', 'https://www.youtube.com/embed/Z2BXbIbY7n8');
     },
 })
