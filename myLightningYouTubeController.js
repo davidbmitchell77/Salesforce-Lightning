2946b11ff0302318c15caf485926c72c5b91bb54
David Mitchell <davidbmitchell77@outlook.com>
2019-04-03 07:21:27 -0700

    Update myLightningYouTubeController.js

--- myLightningYouTubeController.js
+++ myLightningYouTubeController.js
@@ -1,6 +1,7 @@
 ({
     init : function(component, event, helper)
     {
+        document.title('Salesforce: YouTube');
         component.set('v.frameWidth', (window.innerWidth - 10) + 'px');
         component.set('v.frameHeight', (window.innerHeight - 10) + 'px');
         component.set('v.url', 'https://www.youtube.com/embed/Z2BXbIbY7n8');
