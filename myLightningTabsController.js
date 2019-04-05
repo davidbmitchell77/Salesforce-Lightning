a6ae73b036b80552c8ef3ef8d7111a7d3358679e
David Mitchell <davidbmitchell77@outlook.com>
2019-04-05 10:44:59 -0700

    Create myLightningTabsController.js

---
+++ myLightningTabsController.js
@@ -0,0 +1,36 @@
+({
+	next : function(component, event, helper)
+    {
+        var tabId = component.get('v.selectedTabId');
+        switch(tabId)
+        {
+            case 'Tab1':
+                tabId = 'Tab2';
+                break;
+            case 'Tab2':
+                tabId = 'Tab3';
+                break;
+            case 'Tab3':
+                tabId = 'Tab4';
+                break;      
+        }
+        component.set('v.selectedTabId', tabId);
+	},
+	back : function(component, event, helper)
+    {
+        var tabId = component.get('v.selectedTabId');
+        switch(tabId)
+        {
+            case 'Tab4':
+                tabId = 'Tab3';
+                break;
+            case 'Tab3':
+                tabId = 'Tab2';
+                break;
+            case 'Tab2':
+                tabId = 'Tab1';
+                break;
+        }
+        component.set('v.selectedTabId', tabId);
+	},
+})
